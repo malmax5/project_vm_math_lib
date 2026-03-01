884d3cb88ca6b323f13854bb1f86085745e4cd5d
cbf <malmax5@mail.ru>
2026-03-01 15:20:46 +0300

    feat(exceptions): implement a base class of calculator exception

--- include/pvm_math_lib/calculator_exceptions.hpp
+++ include/pvm_math_lib/calculator_exceptions.hpp
@@ -8,5 +8,11 @@ namespace calculator_exceptions
 #include <stdexcept>
 #include <string>
 
+class CalculatorExceptionBase : public std::runtime_error
+{
+public:
+    using std::runtime_error::runtime_error;
+};
+
 } // namespace exceptions
 } // namespace pvm_math_lib
