092b6a10606562a1352ac208e452ca33293f4ebd
cbf <malmax5@mail.ru>
2026-03-01 16:15:44 +0300

    refactor(math): add a message to exceptions in calculator functions

--- include/pvm_math_lib/calculator_math.hpp
+++ include/pvm_math_lib/calculator_math.hpp
@@ -30,7 +30,7 @@ public:
 
         if (__builtin_add_overflow(first, second, &result)) 
         {
-            throw calculator_exceptions::CalculatorExceptionOverflow();
+            throw calculator_exceptions::CalculatorExceptionOverflow("Overflow in add");
         }
 
         return result;
@@ -42,7 +42,7 @@ public:
 
         if (__builtin_sub_overflow(first, second, &result))
         {
-            throw calculator_exceptions::CalculatorExceptionOverflow();
+            throw calculator_exceptions::CalculatorExceptionOverflow("Overflow in sub");
         }
 
         return result;
@@ -54,7 +54,7 @@ public:
 
         if (__builtin_mul_overflow(first, second, &result))
         {
-            throw calculator_exceptions::CalculatorExceptionOverflow();
+            throw calculator_exceptions::CalculatorExceptionOverflow("Overflow in mul");
         }
 
         return result;
@@ -64,7 +64,7 @@ public:
     {
         if (second == 0)
         {
-            throw calculator_exceptions::CalculatorExceptionDivideByZero();
+            throw calculator_exceptions::CalculatorExceptionDivideByZero("Divide by zero");
         }
 
         long long result = first / second;
@@ -76,7 +76,7 @@ public:
     {
         if (exp < 0)
         {
-            throw calculator_exceptions::CalculatorExceptionInvalidOperands();
+            throw calculator_exceptions::CalculatorExceptionInvalidOperands("Power of number by negative exponent");
         }
 
         long long result;
@@ -93,12 +93,12 @@ public:
         {
             if (exp & 1 && __builtin_mul_overflow(resultTmp, num, &resultTmp))
             {
-                throw calculator_exceptions::CalculatorExceptionOverflow();
+                throw calculator_exceptions::CalculatorExceptionOverflow("Overflow in power");
             }
 
             if (__builtin_mul_overflow(num, num, &num))
             {
-                throw calculator_exceptions::CalculatorExceptionOverflow();
+                throw calculator_exceptions::CalculatorExceptionOverflow("Overflow in power");
             }
 
             exp >>= 1;
@@ -113,7 +113,7 @@ public:
     {
         if (n < 0)
         {
-            throw calculator_exceptions::CalculatorExceptionInvalidOperands();
+            throw calculator_exceptions::CalculatorExceptionInvalidOperands("Factorial of negative number");
         }
         else if (n == 0 || n == 1)
         {
@@ -125,7 +125,7 @@ public:
 
         if (__builtin_mul_overflow(n, prev, &result))
         {
-            throw calculator_exceptions::CalculatorExceptionOverflow();
+            throw calculator_exceptions::CalculatorExceptionOverflow("Overflow in factorial");
         }
 
         return result;
